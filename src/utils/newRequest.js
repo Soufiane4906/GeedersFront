506f16fd8a093beff12f8c8612aa4df14b8c6d92
Soufiane4906 <126627087+Soufiane4906@users.noreply.github.com>
2025-01-13 15:36:27 +0100

    Update ip address for deployment

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const newRequest = axios.create({
  // baseURL: "https://geedersapi.onrender.com/api/",
- baseURL: "http://localhost:8800/api",
+ baseURL: "https://46.202.195.73:8800/api",
 
   withCredentials: true,
 });
