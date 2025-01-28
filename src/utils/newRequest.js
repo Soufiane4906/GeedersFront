a4ac8b1b9cecdba63cf54c3944ddf2fca965bbb8
Soufiane4906 <126627087+Soufiane4906@users.noreply.github.com>
2025-01-28 21:01:47 +0100

    using /api/

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const newRequest = axios.create({
  // baseURL: "https://BlaBlaTripapi.onrender.com/api/",
- baseURL: "https://46.202.195.73:8800/api",
+ baseURL: "/api/",
 
 
   withCredentials: true,
