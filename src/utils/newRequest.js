05c6580e686ebb2efedd66c60d171140e6ac328c
Soufiane4906 <126627087+Soufiane4906@users.noreply.github.com>
2025-01-28 03:34:13 +0100

    using http two ports for api backend

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const newRequest = axios.create({
  // baseURL: "https://BlaBlaTripapi.onrender.com/api/",
- baseURL: "https://blablatrip.com/api",
+ baseURL: "http://blablatrip.com/api",
 
 
   withCredentials: true,
