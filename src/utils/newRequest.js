8ad4f768dda1bee59199e3d738091c2d7bc45787
Soufiane4906 <126627087+Soufiane4906@users.noreply.github.com>
2025-04-13 01:01:28 +0100

    fixing filters

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const newRequest = axios.create({
  // baseURL: "https://BlaBlaTripapi.onrender.com/api/",
- baseURL: "https://www.blablatrip.com/api",  // Change to your domain
+   baseURL: "https://www.blablatrip.com/api",  // Change to your domain
    //baseURL: "http://localhost:8800/api",  // Change to your domain
 
   withCredentials: true,
